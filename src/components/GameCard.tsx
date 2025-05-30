46842ebac6ace9dcb270f0be301c309514def3bc
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-05-30 15:48:16 +0300

    Feat: add platforms and metacritic in Game interface and use them in GameCard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Card, Image, Text } from '@chakra-ui/react'
+import { Card, Image, Text, Badge, HStack } from '@chakra-ui/react'
 import React from 'react'
 import type { Game } from '../models/fetch-game-types'
 
@@ -6,20 +6,30 @@ interface Props {
   game: Game
 }
 
-const GameCard: React.FC<Props> = ({game}) => {
-  return  (<Card.Root maxW="sm" overflow="hidden">
-      <Image
-        src={game.background_image}
-        alt={`image of game ${game.name}`}
-        objectFit={"cover"}
-        height="100%"
-      />
-      <Card.Body gap="2">
-        <Card.Title>{game.name}</Card.Title>
-        
-      </Card.Body>
-      
-    </Card.Root>
+function getColors(metacritic: number): { color: string, bg: string } {
+  return metacritic > 90 ? { color: "white", bg: "green" } : { color: "black", bg: "lightgray" }
+}
+
+const GameCard: React.FC<Props> = ({ game }) => {
+  return (<Card.Root maxW="sm" overflow="hidden">
+    <Image
+      src={game.background_image}
+      alt={`image of game ${game.name}`}
+      objectFit={"cover"}
+      height="100%"
+    />
+    <Card.Body>
+      <Card.Title>{game.name}</Card.Title>
+
+    </Card.Body>
+    <Card.Footer>
+      <HStack justifyContent={"space-between"} width="100%">
+        <Text >{game.parent_platforms.map(p => p.platform.name).join("; ")}</Text>
+        <Badge {...getColors(game.metacritic)}>{game.metacritic}</Badge>
+      </HStack>
+    </Card.Footer>
+
+  </Card.Root>
   )
 }
 
--- src/models/fetch-game-types.ts
+++ src/models/fetch-game-types.ts
@@ -1,7 +1,13 @@
+export interface Platform {
+    platform: {id: number, name: string}
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: Platform[];
+    metacritic: number;
 }
 
 export interface GamesResponse {
