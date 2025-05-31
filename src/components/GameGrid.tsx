739d0236ac5fd124d9baf87afd094c88560e8a68
Tomer Peceniski <tomerpeceniski@gmail.com>
2025-05-31 18:40:36 +0300

    refactor: update useGame hook to use react-query for enhace UX and performance

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,10 +4,10 @@ import { Text, SimpleGrid, Spinner } from '@chakra-ui/react'
 import type { FC } from 'react'
 
 const GameGrid: FC = () => {
-    const { data: games, errorMessage, isLoading } = useGame();
+    const { data: games, error, isLoading } = useGame();
 
     return isLoading ? <Spinner /> : (<>
-        {errorMessage ? <Text color="red" fontSize={"2.5rem"}>{errorMessage}</Text> :
+        {error ? <Text color="red" fontSize={"2.5rem"}>{error.message}</Text> :
             <SimpleGrid paddingEnd={2} maxHeight="85vh" overflow="auto" marginTop="2vh" columns={
                 {
                     base: 1,
@@ -15,7 +15,7 @@ const GameGrid: FC = () => {
                     md: 3
                 }
             } gap={5}>
-                {games.map(g => <GameCard key={g.id} game={g} />)}
+                {games?.map(g => <GameCard key={g.id} game={g} />)}
             </SimpleGrid>}
     </>
     )
--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,24 +1,22 @@
 import type { Game } from "../models/fetch-game-types";
-import useFetchData from "./useFetchData";
 import useGameQuery from "../state-management/store";
+import { useQuery } from "@tanstack/react-query";
+import api from '../services/api-client';
+import type DataResponse from "../models/data-response";
 
-export default function useGame(): {
-    data: Game[],
-    errorMessage: string,
-    isLoading: boolean
-} {
+export default function useGame() {
     const gameQuery = useGameQuery(s => s.gameQuery)
 
-    return useFetchData<Game>(
-        "/games",
-        {
+    return useQuery<Game[], Error>({
+        queryKey: ['games', gameQuery],
+        queryFn: () => api.get<DataResponse<Game>>('/xgames', {
             params: {
                 genres: gameQuery.genreName,
-                parent_platforms: gameQuery.platform?.id,
+                ordering: gameQuery.ordering,
                 search: gameQuery.search,
-                ordering: gameQuery.ordering?.value,
+                parent_platforms: gameQuery.platform?.id
             }
-        },
-        [gameQuery]
-    );
+        }).then(res => res.data.results),
+        staleTime: 3600_000
+    })
 }
